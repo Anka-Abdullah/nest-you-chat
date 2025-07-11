7a3664ff08bf43af5ddcbaca6d383bc13b00e53f
Anka Abdullah <anka@Ankas-MacBook-Air.local>
2025-07-11 15:03:49 +0700

    remove messaging module -

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
-import { MessagingModule } from '../messaging/messaging.module';
 import { ChatProcessor } from './chat.processor';
 import { Message, MessageSchema } from './schemas/message.schema';
 import { Conversation, ConversationSchema } from './schemas/conversation.schema';
@@ -14,7 +13,6 @@ import { Conversation, ConversationSchema } from './schemas/conversation.schema'
       { name: Message.name, schema: MessageSchema },
       { name: Conversation.name, schema: ConversationSchema },
     ]),
-    MessagingModule,
   ],
   providers: [ChatService, ChatGateway, ChatProcessor],
   controllers: [ChatController],
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,22 +1,16 @@
  import { Injectable } from '@nestjs/common';
  import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
- import { MessagingService } from '../messaging/messaging.service';
 import { Message, MessageDocument } from './schemas/message.schema';
  
  @Injectable()
  export class ChatService {
    constructor(
      @InjectModel(Message.name) private msgModel: Model<MessageDocument>,
-     private messaging: MessagingService,
    ) {}
  
    async sendMessage(from: string, to: string, content: string) {
-     await this.messaging.publish('chat_messages', {
-       to,
-       from,
-       content,
-     });
+     
     return { from, to, content };
    }
  
